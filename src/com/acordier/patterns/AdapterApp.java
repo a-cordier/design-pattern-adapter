953783605fd698644f2697d39755cf37aa9b39b5
acordier <anton.36ve@gmail.com>
2015-10-16 20:44:50 +0200

    addition to diagram and comments

--- src/com/acordier/patterns/AdapterApp.java
+++ src/com/acordier/patterns/AdapterApp.java
@@ -4,7 +4,7 @@ public class AdapterApp {
 	/*
 	 * Simple, straight forward adapter design pattern example
 	 * A legacy message system needs to adapt to a bit more sophisticated message system
-	 * Adapter pattern will allow us to provide the legacy world to fit new world requirements
+	 * Adapter pattern will allow us to provide the legacy system with the capability to fit new world requirements
 	 */
 	public static void main(String[] args) {
 		LegacyMessage legacyMessage = new LegacyMessage("bob", "a secret message");
